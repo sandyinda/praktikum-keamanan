55cb04470e2401331768beb43886f757344a2170
sandyinda <sandywaru03@gmail.com>
2025-07-06 17:37:35 +0700

    Add files via upload

---
+++ config.php
@@ -0,0 +1,5 @@
+<?php
+$client_id = "900748441195-vumdjm11pco2h6g4jj1qg5dgpomk48ra.apps.googleusercontent.com";
+$client_secret = "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";
+$redirect_uri = "http://localhost/google-oauth-project/callback.php";
+?>
